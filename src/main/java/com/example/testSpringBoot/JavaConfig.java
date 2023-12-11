f9115fe02540f253958286da462ca6379bbe3c74
satan <e-mezrin2012@yandex.ru>
2023-12-11 21:44:29 +0400

    123

--- src/main/java/com/example/testSpringBoot/JavaConfig.java
+++ src/main/java/com/example/testSpringBoot/JavaConfig.java
@@ -1,13 +1,16 @@
 package com.example.testSpringBoot;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 public class JavaConfig {
+    @ConditionalOnProperty
     @Bean
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
+    @ConditionalOnProperty
     @Bean
     public SystemProfile prodProfile() {
         return new ProductionProfile();
